1880e23413feec62cfc00b31a07a129fce21fe58
Nolasco <nolascolunardi@gmail.com>
2024-05-22 08:44:40 -0300

    Primeiro commit com os arquivos do projeto

---
+++ script.js
@@ -0,0 +1,14 @@
+// scripts.js
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
